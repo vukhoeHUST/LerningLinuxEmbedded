618aa033751eb1ade707663eb99bbf1b84376a4f
vukhoeHUST <vu.van.khoe.1997@gmail.com>
2019-08-21 15:43:52 +0700

    mind map OSI

---
+++ Documents/OSI-TCPIP model.mm
@@ -0,0 +1,129 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1566370239507" ID="ID_1841862862" MODIFIED="1566370591953" TEXT="OSI-TCP/IP model">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#ff0000" CREATED="1566370359504" HGAP="97" ID="ID_1663237301" MODIFIED="1566376508905" POSITION="right" TEXT="4. Transport layer" VSHIFT="-22">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1566374433623" ID="ID_166978157" MODIFIED="1566374480900" TEXT="Ch&#x1ee9;c n&#x103;ng">
+<node CREATED="1566374483127" ID="ID_1058404594" MODIFIED="1566374517666" TEXT="&#x110;&#x1ecb;nh &#x111;&#x1ecb;a ch&#x1ec9; PORT">
+<node CREATED="1566374525662" ID="ID_362610316" MODIFIED="1566374675128" TEXT="Sau khi &#x111;&#x1ecb;a ch&#x1ec9; IP v&#xe0; MAC g&#x1eed;i packet &#x111;&#x1ebf;n &#x111;&#xfa;ng thi&#x1ebf;t b&#x1ecb;, &#x111;&#x1ecb;a ch&#x1ec9; PORT s&#x1ebd; &#x111;&#x1b0;a packet &#x111;&#x1ebf;n &#x111;&#xfa;ng ti&#x1ebf;n tr&#xec;nh &#x111;&#xed;ch tr&#xea;n m&#xe1;y t&#xed;nh &#x111;&#xf3;"/>
+<node CREATED="1566374688093" ID="ID_1094647443" MODIFIED="1566375253847" TEXT="&#x110;&#x1ed9; r&#x1ed9;ng: 16 bits"/>
+<node CREATED="1566374692030" ID="ID_317402486" MODIFIED="1566375260273" TEXT="VD: 753"/>
+</node>
+<node CREATED="1566374706205" ID="ID_1099876666" MODIFIED="1566374959954" TEXT="Ph&#xe2;n chia v&#xe0; l&#x1eaf;p l&#x1ea1;i t&#x1ead;p tin">
+<node CREATED="1566374966364" ID="ID_1668744111" MODIFIED="1566375069863" TEXT="M&#x1ed9;t th&#xf4;ng &#x111;i&#x1ec7;p &#x111;&#x1b0;&#x1ee3;c chia nh&#x1ecf; th&#xe0;nh c&#xe1;c ph&#xe2;n &#x111;o&#x1ea1;n v&#xe0; &#x111;&#xe1;nh s&#x1ed1; th&#x1ee9; t&#x1ef1;"/>
+<node CREATED="1566374985828" ID="ID_1432847587" MODIFIED="1566375138002" TEXT="D&#x1ef1;a v&#xe0;o nh&#x1eef;ng s&#x1ed1; thu&#x1ee9; t&#x1ef1; l&#x1edb;p v&#x1ead;n chuy&#x1ec3;n l&#x1eaf;p l&#x1ea1;i ch&#xed;nh x&#xe1;c th&#xf4;ng &#x111;i&#x1ec7;p khi &#x111;&#x1ebf;n &#x111;&#xed;ch v&#xe0; thay th&#x1ebf; c&#xe1;c g&#xf3;i b&#x1ecb; m&#x1ea5;t khi truy&#x1ec1;n"/>
+</node>
+<node CREATED="1566375167235" ID="ID_112709424" MODIFIED="1566375181061" TEXT="&#x110;i&#x1ec1;u khi&#x1ec3;n k&#x1ebf;t n&#x1ed1;i">
+<node CREATED="1566375553547" ID="ID_928788712" MODIFIED="1566375604311" TEXT="L&#x1edb;p v&#x1ead;n chuy&#x1ec3;n kh&#xf4;ng h&#x1b0;&#x1edb;ng k&#x1ebf;t n&#x1ed1;i coi m&#x1ed7;i ph&#xe2;n &#x111;o&#x1ea1;n l&#xe0; m&#x1ed9;t g&#xf3;i &#x111;&#x1ed9;c l&#x1ead;p v&#xe0; &#x111;&#x1b0;a n&#xf3;p &#x111;&#x1ebf;n l&#x1edb;p v&#x1ead;n chuy&#x1ec3;n &#x1edf; m&#xe1;y &#x111;&#xed;ch"/>
+<node CREATED="1566375555451" ID="ID_273767218" MODIFIED="1566375699218" TEXT="L&#x1edb;p v&#x1ead;n chuy&#x1ec3;n h&#x1b0;&#x1edb;ng k&#x1ebf;t n&#x1ed1;i t&#x1ea1;o li&#xea;n k&#x1ebf;t v&#x1edb;i l&#x1edb;p v&#x1ead;n chuy&#x1ec3;n t&#x1ea1;i m&#xe1;y &#x111;&#xed;ch tr&#x1b0;&#x1edb;c khi ph&#xe2;n ph&#x1ed1;i c&#xe1;c packet sau khi d&#x1eef; li&#x1ec7;u &#x111;&#x1b0;&#x1ee3;c truy&#x1ec1;n k&#x1ebf;t n&#x1ed1;i ng&#x1eaf;t"/>
+</node>
+<node CREATED="1566375184340" ID="ID_1606678682" MODIFIED="1566375203320" TEXT="&#x110;i&#x1ec1;u khi&#x1ec3;n lu&#x1ed3;ng">
+<node CREATED="1566375724511" ID="ID_408826675" MODIFIED="1566375778944" TEXT="Gi&#xf4;ngs l&#x1edb;p data link nh&#x1b0;ng ki&#x1ec3;m so&#xe1;t lu&#x1ed3;ng t&#x1eeb; &#x111;&#x1ea7;u &#x111;&#x1ebf;n cu&#x1ed1;i ch&#x1ee9; kh&#xf4;ng ph&#x1ea3;i qua 1 li&#xea;n k&#x1ebf;t &#x111;&#x1a1;n"/>
+</node>
+<node CREATED="1566375188876" ID="ID_1208475867" MODIFIED="1566375197487" TEXT="Ki&#x1ec3;m so&#xe1;t l&#x1ed7;i">
+<node CREATED="1566375784109" ID="ID_1985093080" MODIFIED="1566375860251" TEXT="GI&#x1ed1;ng l&#x1edb;p data link nhuiwng th&#x1ef1;c hi&#x1ec7;n theo qui tr&#xec;nh, l&#x1edb;p v&#x1ead;n chuy&#x1ec3;n g&#x1eed;i &#x111;&#x1ea3;m b&#x1ea3;o r&#x1eb1;ng m&#x1ecd;i th&#xf4;ng tin &#x111;&#x1ebf;n l&#x1edb;p v&#x1ead;n chuy&#x1ec3;n kh&#xf4;ng c&#xf3; l&#x1ed7;i, s&#x1eed;a l&#x1ed7;i th&#xf4;ng qua truy&#x1ec1;n l&#x1ea1;i"/>
+</node>
+</node>
+<node CREATED="1566374436703" ID="ID_267666109" MODIFIED="1566376204107" TEXT="Giao th&#x1ee9;c">
+<node CREATED="1566376214143" ID="ID_1869975128" MODIFIED="1566376221299" TEXT="TCP"/>
+<node CREATED="1566376217119" ID="ID_271546927" MODIFIED="1566376226321" TEXT="UDP"/>
+<node CREATED="1566376231736" ID="ID_1363594700" MODIFIED="1566376234628" TEXT="SCTP"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1566370368921" HGAP="29" ID="ID_1749607630" MODIFIED="1566376446447" POSITION="left" TEXT="7. Application layer" VSHIFT="-11">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1566376271498" ID="ID_1592544476" MODIFIED="1566376275619" TEXT="Ch&#x1ee9;c n&#x103;ng">
+<node CREATED="1566376312547" ID="ID_615841756" MODIFIED="1566376371617" TEXT="Cung c&#x1ea5;p giao di&#x1ec7;n ng&#x1b0;&#x1edd;i d&#xf9;ng"/>
+<node CREATED="1566376375381" ID="ID_1217651252" MODIFIED="1566376418295" TEXT="H&#x1ed7; tr&#x1ee3; c&#xe1;c d&#x1ecb;ch v&#x1ee5; th&#x1b0; &#x111;i&#x1ec7;n t&#x1eed;, truy c&#x1ead;p v&#xe0; truy&#x1ec1;n t&#x1ec7;p t&#x1eeb; xa"/>
+<node CREATED="1566376419383" ID="ID_172859817" MODIFIED="1566376437731" TEXT="Qu&#x1ea3;n l&#xfd; c&#x1a1; s&#x1edf; d&#x1eef; li&#x1ec7;u d&#xf9;ng chung"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1566370373342" HGAP="114" ID="ID_668805834" MODIFIED="1566376515249" POSITION="right" TEXT="3. Network layer" VSHIFT="-5">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1566373837833" ID="ID_108108668" MODIFIED="1566373843255" TEXT="Ch&#x1ee9;c n&#x103;ng">
+<node CREATED="1566373845401" ID="ID_894193160" MODIFIED="1566373970497" TEXT="&#x110;&#x1ecb;nh &#x111;&#x1ecb;a ch&#x1ec9; logical(IP)">
+<node CREATED="1566374260089" ID="ID_108220656" MODIFIED="1566374312630" TEXT="&#x110;&#x1ecb;a ch&#x1ec9; MAC &#x1edf; l&#x1edb;p datalink ch&#x1ec9; x&#x1eed; l&#xfd; &#x111;&#x1b0;&#x1ee3;c &#x1edf; m&#x1ea1;ng c&#x1ee5;c b&#x1ed9;"/>
+<node CREATED="1566374263010" ID="ID_816129523" MODIFIED="1566374353670" TEXT="Khi packet mu&#x1ed1;n &#x111;i qua m&#x1ea1;ng kh&#xe1;c c&#x1ea7;n &#x111;&#x1ebf;n &#x111;&#x1ecb;a ch&#x1ec9; IP"/>
+<node CREATED="1566375291228" ID="ID_1116342992" MODIFIED="1566375336767" TEXT="&#x110;&#x1ed9; r&#x1ed9;ng 32 bits chia th&#xe0;nh 4 nh&#xf3;m"/>
+<node CREATED="1566375316419" ID="ID_1673562747" MODIFIED="1566375354493" TEXT="VD: 192.168.1.3"/>
+</node>
+<node CREATED="1566373853337" ID="ID_820778159" MODIFIED="1566374065988" TEXT="&#x110;&#x1ecb;nh tuy&#x1ebf;n:">
+<node CREATED="1566374069196" ID="ID_958393420" MODIFIED="1566374070267" TEXT=" ch&#x1ec9; ra h&#x1b0;&#x1edb;ng &#x111;i c&#x1ee7;a packet t&#x1eeb; m&#x1ea1;ng ngu&#x1ed3;n &#x111;&#x1ebf;n m&#x1ea1;ng &#x111;&#xed;ch th&#xf4;ng qua c&#xe1;c node trung gian"/>
+</node>
+</node>
+<node CREATED="1566374196954" ID="ID_1834036434" MODIFIED="1566374235168" TEXT="Giao th&#x1ee9;c:">
+<node CREATED="1566374237362" HGAP="27" ID="ID_579348981" MODIFIED="1566374251993" TEXT=" Internet Protocol IP" VSHIFT="3"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1566370378311" ID="ID_485335130" MODIFIED="1566376282186" POSITION="left" TEXT="6. Presentation layer">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1566376021911" ID="ID_650651911" MODIFIED="1566376059292" TEXT="Translation">
+<node CREATED="1566376062111" ID="ID_698023397" MODIFIED="1566376108872" TEXT="Ch&#x1ecb;u tr&#xe1;ch nhi&#x1ec7;m v&#x1ec1; kh&#x1ea3; n&#x103;ng t&#x1b0;&#x1a1;ng t&#xe1;c gi&#x1b0;&#xe3; c&#xe1;c ph&#x1b0;&#x1a1;ng th&#x1ee9;c m&#xe3; h&#xf3;a kh&#xe1;c nhau c&#x1ee7;a b&#xea;n truy&#x1ec1;n v&#xe0; b&#xea;n nh&#x1ead;n"/>
+</node>
+<node CREATED="1566376024278" ID="ID_632389849" MODIFIED="1566376123751" TEXT="M&#xe3; ho&#xe1;">
+<node CREATED="1566376125411" ID="ID_1412832370" MODIFIED="1566376164313" TEXT="&#x110;&#x1ec3; &#x111;&#x1ea3;m b&#x1ea3;o quy&#x1ec1;n ri&#xea;ng t&#x1b0; khi truy&#x1ec1;n tin ra ngo&#xe0;i m&#x1ea1;ng"/>
+</node>
+<node CREATED="1566376168365" ID="ID_697258765" MODIFIED="1566376176346" TEXT="N&#xe9;n d&#x1eef; li&#x1ec7;u"/>
+</node>
+<node COLOR="#ff0000" CREATED="1566370500496" HGAP="90" ID="ID_437074734" MODIFIED="1566374466822" POSITION="right" TEXT="2. Data link layer" VSHIFT="15">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1566372537853" ID="ID_432296236" MODIFIED="1566372543304" TEXT="Ch&#x1ee9;c n&#x103;ng">
+<node CREATED="1566372556502" ID="ID_1699517352" MODIFIED="1566372570855" TEXT="T&#x1ea1;o frames">
+<node CREATED="1566372597854" ID="ID_136710191" MODIFIED="1566372989446" TEXT="Frame l&#xe0; m&#x1ed9;t &#x111;&#x1a1;n v&#x1ecb; truy&#x1ec1;n th&#xf4;ng s&#x1ed1; - ph&#x1ea7;n t&#x1ea3;i mang theo m&#xe0; packet v&#x1ead;n chuy&#x1ec3;n"/>
+<node CREATED="1566372601566" ID="ID_1591016276" MODIFIED="1566372917567" TEXT="L&#x1edb;p data link chia lu&#x1ed3;ng bit nh&#x1ead;n &#x111;&#x1b0;&#x1ee3;c t&#x1eeb; l&#x1edb;p Network th&#xe0;nh c&#xe1;c frame c&#xf3; th&#x1ec3; qu&#x1ea3;n l&#xfd; &#x111;&#x1b0;&#x1ee3;c"/>
+</node>
+<node CREATED="1566372660512" ID="ID_988357416" MODIFIED="1566372709661" TEXT="&#x110;&#x1ecb;nh &#x111;&#x1ecb;a ch&#x1ec9; v&#x1ead;t l&#xfd; (MAC)">
+<node CREATED="1566373401375" ID="ID_801501507" MODIFIED="1566373426627" TEXT="Th&#xea;m header x&#xe1;c &#x111;&#x1ecb;nh &#x111;&#x1ecb;a ch&#x1ec9; g&#x1eed;i v&#xe0; nh&#x1ead;n v&#xe0;o frame"/>
+<node CREATED="1566375368595" ID="ID_637275998" MODIFIED="1566375387872" TEXT="&#x110;&#x1ed9; r&#x1ed9;ng 48 bit chia th&#xe0;nh 12 s&#x1ed1; hexa"/>
+<node CREATED="1566375371611" ID="ID_829906184" MODIFIED="1566375409599" TEXT="VD: 12:AC:3B:4C:07:BD"/>
+</node>
+<node CREATED="1566373372769" ID="ID_656698381" MODIFIED="1566373382392" TEXT="&#x110;i&#x1ec1;u khi&#x1ec3;n lu&#x1ed3;ng">
+<node CREATED="1566373397825" ID="ID_1229035850" MODIFIED="1566373562905" TEXT="N&#x1ebf;u t&#x1ed1;c &#x111;&#x1ed9; nh&#x1ead;n d&#x1eef; li&#x1ec7;u nh&#x1ecf; h&#x1a1;n t&#x1ed1;c &#x111;&#x1ed9; g&#x1eed;i th&#xec; ch&#x1eb7;n ng&#x1b0;&#x1edd;i nh&#x1ead;n"/>
+</node>
+<node CREATED="1566373646936" ID="ID_1881736636" MODIFIED="1566373657852" TEXT="Ki&#x1ec3;m so&#xe1;t l&#x1ed7;i">
+<node CREATED="1566373659992" ID="ID_581306698" MODIFIED="1566373684416" TEXT=" Ph&#xe1;t hi&#x1ec7;n v&#xe0; truy&#x1ec1;n l&#x1ea1;i c&#xe1;c frame l&#x1ed7;i"/>
+</node>
+<node CREATED="1566373693006" ID="ID_1298938401" MODIFIED="1566373716585" TEXT="Ki&#x1ec3;m so&#xe1;t truy c&#x1ead;p">
+<node CREATED="1566373718733" ID="ID_151048412" MODIFIED="1566373749912" TEXT="x&#xe1;c &#x111;&#x1ecb;nh quy&#x1ec1;n ki&#x1ec3;m so&#xe1;t li&#xea;n k&#x1ebf;t khi c&#xf3; nhi&#x1ec1;u thi&#x1ebf;t b&#x1ecb; k&#x1ebf;t n&#xf3;i v&#x1edb;i li&#xea;n k&#x1ebf;t"/>
+</node>
+</node>
+<node CREATED="1566374141867" ID="ID_815872425" MODIFIED="1566374170016" TEXT="Giao th&#x1ee9;c: Ethernet IEEE 802.3"/>
+</node>
+<node COLOR="#ff0000" CREATED="1566370505001" HGAP="44" ID="ID_545385737" MODIFIED="1566376451327" POSITION="left" TEXT="5. Session layer" VSHIFT="22">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1566375878388" ID="ID_1852171622" MODIFIED="1566375968113" TEXT="Cho ph&#xe9;p 2 h&#x1ec7; th&#x1ed1;ng nh&#x1ead;p v&#xe0;o m&#x1ed9;t h&#x1ed9;p tho&#x1ea1;i, cho ph&#xe9;p giao ti&#x1ebf;p gi&#x1eef;a 2 process &#x1edf; ch&#x1ebf; &#x111;&#x1ecd; b&#xe1;n song c&#xf4;ng ho&#x1eb7;c song c&#xf4;ng"/>
+<node CREATED="1566375881220" ID="ID_1248090793" MODIFIED="1566376014240" TEXT="&#x110;&#x1ed3;ng b&#x1ed9; h&#xf3;a: cho ph&#xe9;p 1 process th&#xea;m checkpoints v&#xe0;o lu&#x1ed3;ng d&#x1eef; li&#x1ec7;u"/>
+</node>
+<node COLOR="#ff0000" CREATED="1566370836395" HGAP="114" ID="ID_1354741406" MODIFIED="1566373766787" POSITION="right" TEXT=" 1. Physiccal layer" VSHIFT="20">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1566371170284" HGAP="25" ID="ID_205092011" MODIFIED="1566371387436" TEXT="X&#xe1;c &#x111;&#x1ecb;nh lo&#x1ea1;i m&#xe3; h&#xf3;a t&#xed;n hi&#x1ec7;u" VSHIFT="-12"/>
+<node CREATED="1566371237653" ID="ID_1303378427" MODIFIED="1566372148001" TEXT="T&#x1ed1;c &#x111;&#x1ed9; truy&#x1ec1;n nh&#x1ead;n bit - baud rate"/>
+<node CREATED="1566371240365" ID="ID_770349042" MODIFIED="1566372197817" TEXT="&#x110;&#x1ed3;ng b&#x1ed9; h&#xf3;a c&#xe1;c bit gi&#x1eef;a b&#xea;n g&#x1eed;i v&#xe0; nh&#x1ead;n">
+<node CREATED="1566372151174" ID="ID_1768308213" MODIFIED="1566372216401" TEXT="&#x110;&#x1ed3;ng b&#x1ed9; clocks bit"/>
+</node>
+<node CREATED="1566371245093" ID="ID_279797931" MODIFIED="1566371320298" TEXT="C&#x1ea5;u h&#xec;nh &#x111;&#x1b0;&#x1edd;ng d&#xe2;y"/>
+<node CREATED="1566371249100" ID="ID_1983359511" MODIFIED="1566371342664" TEXT="C&#x1ea5;u tr&#xfa;c li&#xea;n k&#x1ebf;t v&#x1ead;t l&#xfd;">
+<node CREATED="1566371555949" ID="ID_1000071105" MODIFIED="1566371583243" TEXT="li&#xea;n k&#x1ebf;t sao"/>
+<node CREATED="1566371559028" ID="ID_399331588" MODIFIED="1566371591058" TEXT="li&#xea;n k&#x1ebf;t v&#xf2;ng"/>
+<node CREATED="1566371561373" ID="ID_1189174842" MODIFIED="1566371596010" TEXT="li&#xea;n k&#x1ebf;t l&#x1b0;&#x1edb;i"/>
+<node CREATED="1566371563957" ID="ID_1113100965" MODIFIED="1566371606669" TEXT="li&#xea;n k&#x1ebf;t bus"/>
+</node>
+<node CREATED="1566371343964" ID="ID_1646057940" MODIFIED="1566371456269" TEXT="x&#xe1;c &#x111;&#x1ecb;nh ch&#x1ebf; &#x111;&#x1ed9; truy&#x1ec1;n gi&#x1eef;a 2 device">
+<node CREATED="1566371413757" ID="ID_1177833596" MODIFIED="1566371446242" TEXT="&#x110;&#x1a1;n gi&#x1ea3;n">
+<node CREATED="1566371536637" ID="ID_80094425" MODIFIED="1566371549385" TEXT="Ch&#x1ec9; truy&#x1ec1;n ho&#x1eb7;c ch&#x1ec9; nh&#x1ead;n"/>
+</node>
+<node CREATED="1566371422181" ID="ID_141045542" MODIFIED="1566371458152" TEXT="B&#xe1;n song c&#xf4;ng">
+<node CREATED="1566371507621" ID="ID_72724439" MODIFIED="1566371532497" TEXT="Truy&#x1ec1;n nh&#x1ead;n kh&#xf4;ng &#x111;&#x1ed3;ng th&#x1edd;i"/>
+</node>
+<node CREATED="1566371425156" ID="ID_1979056792" MODIFIED="1566371477897" TEXT="Song c&#xf4;ng">
+<node CREATED="1566371482565" HGAP="21" ID="ID_82857186" MODIFIED="1566371502492" TEXT="Truy&#x1ec1;n nh&#x1ead;n &#x111;&#x1ed3;ng th&#x1edd;i" VSHIFT="2"/>
+</node>
+</node>
+<node CREATED="1566374398449" ID="ID_1819314345" MODIFIED="1566374405410" TEXT="Ph&#x1ea7;n c&#x1ee9;ng">
+<node CREATED="1566374408008" ID="ID_1754222649" MODIFIED="1566374422988" TEXT="C&#xe1;p xo&#x1eaf;n"/>
+<node CREATED="1566374410273" ID="ID_896061329" MODIFIED="1566374416043" TEXT="C&#xe1;p quang"/>
+</node>
+</node>
+</node>
+</map>
